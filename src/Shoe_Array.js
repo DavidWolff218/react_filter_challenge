12587d4ddc48b2febde374b2eb20b143a33c8275
DavidWolff218 <dcwolff218@gmail.com>
2021-05-20 12:31:58 -0500

    made components and shoe_array

---
+++ src/Shoe_Array.js
@@ -0,0 +1,76 @@
+let shoeArray = [
+    {
+        id: 1,
+        name: "shoe 1",
+        brand: "Nike",
+        Color: "red"
+    },
+    {
+        id: 2,
+        name: "shoe 2",
+        brand: "Nike",
+        Color: "black"
+    }, 
+    {
+        id: 3,
+        name: "shoe 3",
+        brand: "Nike",
+        Color: "white"
+    },
+    {
+        id: 4,
+        name: "shoe 4",
+        brand: "Nike",
+        Color: "red"
+    },
+    {
+        id: 5,
+        name: "shoe 5",
+        brand: "Adidas",
+        Color: "red"
+    },
+    {
+        id: 6,
+        name: "shoe 6",
+        brand: "Adidas",
+        Color: "black"
+    },
+    {
+        id: 7,
+        name: "shoe 7",
+        brand: "Adidas",
+        Color: "white"
+    },
+    {
+        id: 8,
+        name: "shoe 8",
+        brand: "Adidas",
+        Color: "white"
+    },
+    {
+        id: 9,
+        name: "shoe 9",
+        brand: "Reebok",
+        Color: "red"
+    },
+    {
+        id: 10,
+        name: "shoe 10",
+        brand: "Reebok",
+        Color: "black"
+    },
+    {
+        id: 11,
+        name: "shoe 11",
+        brand: "Reebok",
+        Color: "black"
+    },
+    {
+        id: 12,
+        name: "shoe 12",
+        brand: "Reebok",
+        Color: "white"
+    }
+]
+
+export default shoeArray
---
+++ src/components/Container.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import shoeArray from '../Shoe_Array'
+
+export default class Container extends Component {
+
+    state = {
+
+    }
+
+   
+   
+    render() {
+        console.log("hello", shoeArray)
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
