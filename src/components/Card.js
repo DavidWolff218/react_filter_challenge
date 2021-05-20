5860ea15832e1d42356c4b7ab8825d4d55a207ce
DavidWolff218 <dcwolff218@gmail.com>
2021-05-20 13:03:22 -0500

    rendering shoes

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,9 @@ export default class Card extends Component {
     render() {
         return (
             <div>
-                
+                <h3>Name: {this.props.shoe.name}</h3>
+                <h4>Brand: {this.props.shoe.brand}</h4>
+                <h4>Color: {this.props.shoe.color}</h4>
             </div>
         )
     }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import shoeArray from '../Shoe_Array'
+import Card from './Card'
 
 export default class Container extends Component {
 
     state = {
-
+        shoes: []
     }
 
+   componentDidMount(){
+       this.setState({
+           shoes: shoeArray
+       })
+   }
    
-   
+   shoesToRender = () => {
+      return this.state.shoes.map( shoe => <Card shoe={shoe} key={shoe.id}/>)
+   }
+
     render() {
-        console.log("hello", shoeArray)
+        console.log(this.state)
         return (
             <div>
-                
+                {this.shoesToRender()}
             </div>
         )
     }
